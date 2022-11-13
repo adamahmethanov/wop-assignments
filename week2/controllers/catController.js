894bd00f41f079c39b3dddf415c2fe8c4a8e34e2
Adam <adam.ahmethanov@metropolia.fi>
2022-11-13 15:04:36 +0000

    update

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,38 +1,131 @@
 'use strict';
 // catController
-const {getCat, getAllCats, addCat} = require('../models/catModel');
+const {getCat, getAllCats, addCat, updateCat, deleteCat} = require(
+    '../models/catModel');
+const {httpError} = require('../utils/errors');
+const {validationResult} = require('express-validator');
 
-const cat_list_get = async (req, res) => {
-  const kissat = await getAllCats();
-  res.json(kissat);
-};
-
-const cat_get = async (req, res) => {
-  const cat = await getCat(req.params.id);
-  if(cat.length > 0) {
-    res.json(cat.pop());
-  } else {
-    res.send('virhe');
+const cat_list_get = async (req, res, next) => {
+  try {
+    const kissat = await getAllCats(next);
+    if (kissat.length < 1) {
+      next(httpError('No cats found', 404));
+      return;
+    }
+    res.json(kissat);
+  } catch (e) {
+    console.error('cat_list_get', e.message);
+    next(httpError('Internal server error', 500));
   }
 };
 
-const cat_post = async (req, res) => {
-  console.log('cat_post', req.body, req.file);
-  const data = [
-    req.body.name,
-    req.body.birthdate,
-    req.body.weight,
-    req.body.owner,
-    req.file.filename,
-  ];
+const cat_get = async (req, res, next) => {
+  try {
+    const cat = await getCat(req.params.id, next);
+    if (cat.length < 1) {
+      next(httpError('No cat found', 404));
+      return;
+    }
+    res.json(cat.pop());
+  } catch (e) {
+    console.error('cat_get', e.message);
+    next(httpError('Internal server error', 500));
+  }
+};
 
-  const result = await addCat(data);
-  console.log('addCat', result);
-  res.send('Cat post done.');
+const cat_post = async (req, res, next) => {
+  try {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Error messages can be returned in an array using `errors.array()`.
+      console.error('user_post validation', errors.array());
+      next(httpError('Invalid data', 400));
+      return;
+    }
+
+    console.log('cat_post', req.body, req.file);
+    const data = [
+      req.body.name,
+      req.body.birthdate,
+      req.body.weight,
+      req.body.owner,
+      req.file.filename,
+    ];
+
+    const result = await addCat(data, next);
+    if (result.affectedRows < 1) {
+      next(httpError('Invalid data', 400));
+      return;
+    }
+    res.json({
+      message: 'cat added',
+      cat_id: result.insertId,
+    });
+  } catch (e) {
+    console.error('cat_post', e.message);
+    next(httpError('Internal server error', 500));
+  }
+};
+
+const cat_put = async (req, res, next) => {
+  try {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Error messages can be returned in an array using `errors.array()`.
+      console.error('user_post validation', errors.array());
+      next(httpError('Invalid data', 400));
+      return;
+    }
+
+    const data = [
+      req.body.name,
+      req.body.birthdate,
+      req.body.weight,
+      req.body.owner,
+      req.body.id,
+    ];
+
+    const result = await updateCat(data, next);
+    if (result.affectedRows < 1) {
+      next(httpError('No cat modified', 400));
+      return;
+    }
+
+    res.json({
+      message: 'cat modified',
+    });
+  } catch (e) {
+    console.error('cat_put', e.message);
+    next(httpError('Internal server error', 500));
+  }
+};
+
+const cat_delete = async (req, res, next) => {
+  try {
+    const result = await deleteCat(req.params.id, next);
+    if (result.affectedRows < 1) {
+      next(httpError('No cat deleted', 400));
+      return;
+    }
+    res.json({
+      message: 'cat deleted',
+    });
+  } catch (e) {
+    console.error('delete', e.message);
+    next(httpError('Internal server error', 500));
+  }
 };
 
 module.exports = {
   cat_list_get,
   cat_get,
   cat_post,
+  cat_put,
+  cat_delete,
 };
